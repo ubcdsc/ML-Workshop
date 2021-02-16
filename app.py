a783744eee9c9de56fd963ee5c7ecc430e9cedcd
Gagan Bhatia <gagan3012@users.noreply.github.com>
2021-02-16 03:37:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import base64
-
 import streamlit as st
 from transformers import pipeline
 import requests
@@ -11,11 +9,12 @@ st.sidebar.header("""
 © ML Workshop By DSC UBC
 """)
 st.sidebar.markdown(
-    "This app allows users to input text of their choice using the tools provided, and ask questions with the answer being extracted from the text.")
+    "This app allows users to input text of their choice using the tools provided, and ask questions with the answer "
+    "being extracted from the text.")
 st.sidebar.markdown(
-    "_When running the app the first time, it may take some time to initialise due to the requirements needing to be downloaded._")
+    "_When running the app the first time, it may take some time to initialise due to the requirements needing to be "
+    "downloaded._")
 tool = st.sidebar.selectbox("Tool", ["Website Q&A", "Sentiment Analysis"])
-
 
 
 @st.cache(suppress_st_warning=True)
@@ -35,7 +34,7 @@ def generatesentiment(text):
 def website_qna():
     st.write("# Website QnA")
     user_input = st.text_input("Website Link:", value="https://en.wikipedia.org/wiki/Machine_learning")
-    question = st.text_input("Question:",value="What is Machine Learning?")
+    question = st.text_input("Question:", value="What is Machine Learning?")
 
     if st.button("Get Answer"):
         scraped_data = requests.get(user_input)
@@ -53,12 +52,13 @@ def website_qna():
         st.header("Answer")
         st.write(answer)
 
+
 def sentiment():
     st.write("# Sentiment Analysis")
     user_input = st.text_input("Enter Text")
 
     if st.button('Get Sentiment'):
-        answer= generatesentiment(user_input)
+        answer = generatesentiment(user_input)
         st.header("Answer")
         st.write(answer)
 
@@ -68,4 +68,3 @@ if tool == "Website Q&A":
 
 if tool == "Sentiment Analysis":
     sentiment()
-
